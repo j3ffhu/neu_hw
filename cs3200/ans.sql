8ed15fe2aec396f1cc3440b36b3beb65732ec411
j3ffhu <j3ffhu@gmail.com>
2023-10-24 21:15:51 -0400

    Update ans.sql

--- cs3200/ans.sql
+++ cs3200/ans.sql
@@ -2,6 +2,12 @@
 
 
 -- e: What is the most liked tweet?
+select * from tweet 
+where tweet_id in (
+   select tweet_id from (
+              select tweet_id  from `like` group by tweet_id  order by count(user_id)  desc  LIMIT 1) 
+   as   top_tweet  
+) order by created desc;
 
 -- f: home timeline.
 
