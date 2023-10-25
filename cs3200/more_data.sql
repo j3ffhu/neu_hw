552e3c9edc559bf1a8e72272d3a7d7539a9d233a
j3ffhu <j3ffhu@gmail.com>
2023-10-24 22:00:14 -0400

    Create more_data.sql

---
+++ cs3200/more_data.sql
@@ -0,0 +1,79 @@
+
+
+INSERT INTO `mydb`.`like`
+(`tweet_id`,
+`user_id`)
+VALUES 
+(3, 1),
+(4, 2),
+(5, 3),
+(2, 1),
+(2, 2),
+(2, 3),
+(2, 4),
+(2, 5);
+
+
+INSERT INTO `mydb`.`tweet`
+( 
+`user_id`,
+`content`,
+`created`)
+VALUES
+( 
+4,
+'What is the meaning of this Symbol?',
+now());
+
+INSERT INTO `mydb`.`tweet`
+( 
+`user_id`,
+`content`,
+`created`)
+VALUES
+( 
+1,
+'Love #NEU',
+now());
+
+
+INSERT INTO `mydb`.`tweet`
+( 
+`user_id`,
+`content`,
+`created`)
+VALUES
+( 
+1,
+'Fall coming to  #NEU',
+now());
+
+INSERT INTO `mydb`.`tweet`
+( 
+`user_id`,
+`content`,
+`created`)
+VALUES
+( 
+5,
+'It is a good day?',
+now());
+
+
+INSERT INTO `mydb`.`tweet`
+( 
+`user_id`,
+`content`,
+`created`)
+VALUES
+( 
+4,
+'Fall coming to  #NEU',
+now());
+
+
+
+SELECT * FROM mydb.hashtag;
+INSERT INTO hashtagtweet (hashtag_id, tweet_id)
+select 1,  tweet_id from tweet
+where   content like '%#NEU%'
