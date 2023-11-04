776946075696f85ec959a6a0b15aa6aadc029f18
j3ffhu <j3ffhu@gmail.com>
2023-11-03 20:58:58 -0400

    Create fastlane.sql

---
+++ fastlane.sql
@@ -0,0 +1,28 @@
+ 
+	
+	select * from  "Assembly"
+	
+	SELECT  *
+	FROM fastline."Manufacturer";
+	
+		SELECT  *
+	FROM fastline."Manufacturer";
+	
+		SELECT  *
+	FROM fastline."User";
+	
+	
+	INSERT INTO  fastline."Material"(
+	"materialId", 
+	name ,
+	"wbsElementId",
+	"dateCreated",
+	"userCreatedId",
+	"status",
+	"materialTypeName",
+	"manufacturerName", "manufacturerPartNumber", "quantity", "price",  "subtotal", "linkUrl", "notes"  )
+	VALUES ('100', 'book', 1, current_timestamp, 9, 'UNORDERED', 'materialTypeName', 'ABC', 100,2000, 
+			99, 2000000, 'http.rul', 'great note')  ;
+	
+	
+	
