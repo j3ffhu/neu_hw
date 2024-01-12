afa4dc8b9b7214d9e5ae7400d36a39773a9fadd1
j3ffhu <j3ffhu@gmail.com>
2024-01-11 19:08:01 -0800

    Update Message.java

--- 3700/Message.java
+++ 3700/Message.java
@@ -10,6 +10,7 @@ public class Message {
 	@JsonProperty("id")
 	public String id;
 
+	// hello, start, guess, retry.. bye error
 	@JsonProperty("type")
 	public String type;
 
@@ -19,17 +20,30 @@ public class Message {
 	@JsonProperty("word")
 	public String word;
 
+	
+	// guesses: max 500
 	@JsonProperty("guesses")
 	public List<Guess> guesses;
+
+	@JsonProperty("flag")
+	public String flag;
+	
+	@JsonProperty("message")
+	public String message;
+
 }
 
 class Guess {
 
-	
-	//  You will note that all words at exactly 5 characters long, and all words are lowercase
+	// exactly 5 characters long, and all words are lowercase
+	// Guesses are case-sensitive
 	@JsonProperty("word")
 	public String word;
 
+ 
+	// 0 Letter does not appear in the secret word
+	// 1 Letter appears in the secret word, but not in this position
+	// 2 Letter appears in the secret word in this position - dont change
 	@JsonProperty("marks")
 	public List<Integer> marks;
 
